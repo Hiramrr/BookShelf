8fab553ee8ec3ae3795665141ffc84d20f186599
hiramrr <sasuke_77.23@hotmail.com>
2025-06-02 23:13:56 -0600

    Base de datossss

--- BaseDatos_Oracle/Base_Oracle.sql
+++ BaseDatos_Oracle/Base_Oracle.sql
@@ -1,5 +1,5 @@
 --------------------------------------------------------
--- Archivo creado  - viernes-mayo-30-2025
+-- Archivo creado  - lunes-junio-02-2025
 --------------------------------------------------------
 --------------------------------------------------------
 --  DDL for Type CATEGORIA_T
@@ -20,13 +20,13 @@ CREATE OR REPLACE EDITIONABLE TYPE "BIBLIOTECA"."CATEGORIA_T" AS OBJECT (nombre 
 --------------------------------------------------------
 
   CREATE OR REPLACE EDITIONABLE TYPE "BIBLIOTECA"."LIBRO_T" AS OBJECT (id int, titulo VARCHAR(80), autor VARCHAR(50),editorial varchar(50), num_copias int, sinopsis varchar(1000),categorias Categoria_tab_t);
-
+a
 /
 --------------------------------------------------------
 --  DDL for Type PRESTAMO_T
 --------------------------------------------------------
 
-  CREATE OR REPLACE EDITIONABLE TYPE "BIBLIOTECA"."PRESTAMO_T" AS OBJECT (id int, fecha_solicitud date, fecha_recogida date, estado varchar(50), usuario REF Usuario_t, libro REF Libro_t);
+  CREATE OR REPLACE EDITIONABLE TYPE "BIBLIOTECA"."PRESTAMO_T" AS OBJECT (id int, fecha_solicitud date, fecha_devolucion date, estado int, id_usuario int, id_libro int);
 
 /
 --------------------------------------------------------
@@ -73,10 +73,16 @@ CREATE TABLE "BIBLIOTECA"."LIBROS" OF "BIBLIOTECA"."LIBRO_T"
 
 CREATE TABLE "BIBLIOTECA"."PRESTAMOS" OF "BIBLIOTECA"."PRESTAMO_T"
     OIDINDEX  ( PCTFREE 10 INITRANS 2 MAXTRANS 255
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1
+  BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
   TABLESPACE "USERS" )
- SEGMENT CREATION DEFERRED
+ SEGMENT CREATION IMMEDIATE
   PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255
  NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1
+  BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
   TABLESPACE "USERS" ;
 --------------------------------------------------------
 --  DDL for Table RESEÑAS
@@ -136,24 +142,27 @@ Insert into BIBLIOTECA.LIBROS (ID,TITULO,AUTOR,EDITORIAL,NUM_COPIAS,SINOPSIS,CAT
 Insert into BIBLIOTECA.LIBROS (ID,TITULO,AUTOR,EDITORIAL,NUM_COPIAS,SINOPSIS,CATEGORIAS) values (312564,'prueba123','miyu','dhejw',5,'fhjgdshj',BIBLIOTECA.CATEGORIA_TAB_T(BIBLIOTECA.CATEGORIA_T('Alegre')));
 Insert into BIBLIOTECA.LIBROS (ID,TITULO,AUTOR,EDITORIAL,NUM_COPIAS,SINOPSIS,CATEGORIAS) values (21321312,'miyu','coco','coco',22,'oeowiewoiew',BIBLIOTECA.CATEGORIA_TAB_T(BIBLIOTECA.CATEGORIA_T('Educativo')));
 Insert into BIBLIOTECA.LIBROS (ID,TITULO,AUTOR,EDITORIAL,NUM_COPIAS,SINOPSIS,CATEGORIAS) values (1232,'prueba libro con foto','prueba','prueba',44,'esto es una prueba',BIBLIOTECA.CATEGORIA_TAB_T(BIBLIOTECA.CATEGORIA_T('Prueba')));
+Insert into BIBLIOTECA.LIBROS (ID,TITULO,AUTOR,EDITORIAL,NUM_COPIAS,SINOPSIS,CATEGORIAS) values (31232121,'Punpun','inio asano','miyu',20,'Punpun',BIBLIOTECA.CATEGORIA_TAB_T(BIBLIOTECA.CATEGORIA_T('Recuentos de la vida'), BIBLIOTECA.CATEGORIA_T('Drama'), BIBLIOTECA.CATEGORIA_T('Triste')));
 Insert into BIBLIOTECA.LIBROS (ID,TITULO,AUTOR,EDITORIAL,NUM_COPIAS,SINOPSIS,CATEGORIAS) values (432432,'3782','312321','312321',312312,'312312',BIBLIOTECA.CATEGORIA_TAB_T(BIBLIOTECA.CATEGORIA_T('Alegre')));
-Insert into BIBLIOTECA.LIBROS (ID,TITULO,AUTOR,EDITORIAL,NUM_COPIAS,SINOPSIS,CATEGORIAS) values (103,'Oracle','Miyu Maldonado','Coco',5,'Este libro es una introduccion a Oracle',BIBLIOTECA.CATEGORIA_TAB_T(BIBLIOTECA.CATEGORIA_T('Drama'), BIBLIOTECA.CATEGORIA_T('Educativo')));
+Insert into BIBLIOTECA.LIBROS (ID,TITULO,AUTOR,EDITORIAL,NUM_COPIAS,SINOPSIS,CATEGORIAS) values (103,'Oracle editado','Miyu Maldonado','Coco',5,'Este libro es una introduccion a Oracle',BIBLIOTECA.CATEGORIA_TAB_T(BIBLIOTECA.CATEGORIA_T('Drama'), BIBLIOTECA.CATEGORIA_T('Educativo')));
 Insert into BIBLIOTECA.LIBROS (ID,TITULO,AUTOR,EDITORIAL,NUM_COPIAS,SINOPSIS,CATEGORIAS) values (6666,'Coco prueba mongo','hiram','uv',1,'Esto es una prueba para ver si se guarda la ruta de la imagen en mongo',BIBLIOTECA.CATEGORIA_TAB_T(BIBLIOTECA.CATEGORIA_T('Drama')));
 Insert into BIBLIOTECA.LIBROS (ID,TITULO,AUTOR,EDITORIAL,NUM_COPIAS,SINOPSIS,CATEGORIAS) values (101,'Miyu el huerfanito','Coco UV','SEV',5,'Una novela dramatica que narra la vida de miyu',BIBLIOTECA.CATEGORIA_TAB_T(BIBLIOTECA.CATEGORIA_T('Drama'), BIBLIOTECA.CATEGORIA_T('Novela')));
 Insert into BIBLIOTECA.LIBROS (ID,TITULO,AUTOR,EDITORIAL,NUM_COPIAS,SINOPSIS,CATEGORIAS) values (102,'Introduccion a base de datos','Edgar','UV',3,'Introduccion a las bases de datos objeto relacional.',BIBLIOTECA.CATEGORIA_TAB_T(BIBLIOTECA.CATEGORIA_T('Alegre'), BIBLIOTECA.CATEGORIA_T('Educativo')));
 Insert into BIBLIOTECA.LIBROS (ID,TITULO,AUTOR,EDITORIAL,NUM_COPIAS,SINOPSIS,CATEGORIAS) values (978142158,'Goodnight Punpun','Inio Asano','Viz Media',10,'Punpun mira el mundo con los ojos de un pollito, porque tal vez así puede soportar que su amado padre sea un alcohólico violento; su madre, una víctima golpeada; y el amor de su vida, una compañera de clase que se marcha lejos',BIBLIOTECA.CATEGORIA_TAB_T(BIBLIOTECA.CATEGORIA_T('Drama'), BIBLIOTECA.CATEGORIA_T('Recuentos de la vida')));
-Insert into BIBLIOTECA.LIBROS (ID,TITULO,AUTOR,EDITORIAL,NUM_COPIAS,SINOPSIS,CATEGORIAS) values (6075509,'La hipótesis del amor','Ali Hazelwood','Alianza de Novela',5,'La hipótesis del amor sigue a Olive, personaje principal, en su intento de convencer a su mejor amiga, Anh, de que piense que su vida amorosa va muy bien. Para convencer a Anh, finge salir con su profesor, Adam. Olive y Adam intentan convencer a todos los que les rodean de que están enamorados. Pero mientras convencen a todos, olvidan que se supone que sus sentimientos son falsos. ¿Cuánto tiempo les tomará a Olive y Adam darse cuenta de que ya no quieren fingir?',BIBLIOTECA.CATEGORIA_TAB_T(BIBLIOTECA.CATEGORIA_T('Romance')));
+Insert into BIBLIOTECA.LIBROS (ID,TITULO,AUTOR,EDITORIAL,NUM_COPIAS,SINOPSIS,CATEGORIAS) values (6075509,'La hipótesis del amor','Ali Hazelwood','Alianza de Novela',4,'La hipótesis del amor sigue a Olive, personaje principal, en su intento de convencer a su mejor amiga, Anh, de que piense que su vida amorosa va muy bien. Para convencer a Anh, finge salir con su profesor, Adam. Olive y Adam intentan convencer a todos los que les rodean de que están enamorados. Pero mientras convencen a todos, olvidan que se supone que sus sentimientos son falsos. ¿Cuánto tiempo les tomará a Olive y Adam darse cuenta de que ya no quieren fingir?',BIBLIOTECA.CATEGORIA_TAB_T(BIBLIOTECA.CATEGORIA_T('Romance')));
 Insert into BIBLIOTECA.LIBROS (ID,TITULO,AUTOR,EDITORIAL,NUM_COPIAS,SINOPSIS,CATEGORIAS) values (3165267,'Miyucoco','Coco','Miyuki',4,'Blanca',BIBLIOTECA.CATEGORIA_TAB_T(BIBLIOTECA.CATEGORIA_T('Novela'), BIBLIOTECA.CATEGORIA_T('Drama'), BIBLIOTECA.CATEGORIA_T('Coco')));
 Insert into BIBLIOTECA.LIBROS (ID,TITULO,AUTOR,EDITORIAL,NUM_COPIAS,SINOPSIS,CATEGORIAS) values (21321,'Prueba 55','Coco','miyu',444,'prueba de subir libro',BIBLIOTECA.CATEGORIA_TAB_T(BIBLIOTECA.CATEGORIA_T('Educativo'), BIBLIOTECA.CATEGORIA_T('Alegre')));
 REM INSERTING into BIBLIOTECA.PRESTAMOS
 SET DEFINE OFF;
+Insert into BIBLIOTECA.PRESTAMOS (ID,FECHA_SOLICITUD,FECHA_DEVOLUCION,ESTADO,ID_USUARIO,ID_LIBRO) values (19223,to_date('02/06/25','DD/MM/RR'),to_date('11/06/25','DD/MM/RR'),1,41644,6075509);
+Insert into BIBLIOTECA.PRESTAMOS (ID,FECHA_SOLICITUD,FECHA_DEVOLUCION,ESTADO,ID_USUARIO,ID_LIBRO) values (24262,to_date('02/06/25','DD/MM/RR'),to_date('11/06/25','DD/MM/RR'),0,41644,31232121);
 REM INSERTING into BIBLIOTECA."RESEÑAS"
 SET DEFINE OFF;
 REM INSERTING into BIBLIOTECA.USUARIO
 SET DEFINE OFF;
 REM INSERTING into BIBLIOTECA.USUARIOS
 SET DEFINE OFF;
-Insert into BIBLIOTECA.USUARIOS (ID,NOMBRE,CORREO,"CONTRASEÑA") values (41644,'Coco','miyu@gmail.com','123');
+Insert into BIBLIOTECA.USUARIOS (ID,NOMBRE,CORREO,"CONTRASEÑA") values (41644,'Miyu','miyu@gmail.com','123');
 Insert into BIBLIOTECA.USUARIOS (ID,NOMBRE,CORREO,"CONTRASEÑA") values (64745,'hiram','123@gmail.com','123');
 Insert into BIBLIOTECA.USUARIOS (ID,NOMBRE,CORREO,"CONTRASEÑA") values (39535,'Hiram','manzanita77723@gmail.com','miyu123');
 Insert into BIBLIOTECA.USUARIOS (ID,NOMBRE,CORREO,"CONTRASEÑA") values (34480,'Jazmín','miyu123@gmail.com','123');
@@ -162,18 +171,19 @@ Insert into BIBLIOTECA.USUARIOS (ID,NOMBRE,CORREO,"CONTRASEÑA") values (55580,'
 REM INSERTING into BIBLIOTECA.TODOSLIBROS
 SET DEFINE OFF;
 Insert into BIBLIOTECA.TODOSLIBROS (ID,TITULO,AUTOR,EDITORIAL,NUM_COPIAS,SINOPSIS,CATEGORIAS) values (21321,'Prueba 55','Coco','miyu',444,'prueba de subir libro','Educativo, Alegre');
+Insert into BIBLIOTECA.TODOSLIBROS (ID,TITULO,AUTOR,EDITORIAL,NUM_COPIAS,SINOPSIS,CATEGORIAS) values (31232121,'Punpun','inio asano','miyu',20,'Punpun','Recuentos de la vida, Drama, Triste');
 Insert into BIBLIOTECA.TODOSLIBROS (ID,TITULO,AUTOR,EDITORIAL,NUM_COPIAS,SINOPSIS,CATEGORIAS) values (102,'Introduccion a base de datos','Edgar','UV',3,'Introduccion a las bases de datos objeto relacional.','Alegre, Educativo');
 Insert into BIBLIOTECA.TODOSLIBROS (ID,TITULO,AUTOR,EDITORIAL,NUM_COPIAS,SINOPSIS,CATEGORIAS) values (324432,'Otra vez una prueba de mongo','Miyu','prueba',12,'Prueba de mongoo','Educativo');
 Insert into BIBLIOTECA.TODOSLIBROS (ID,TITULO,AUTOR,EDITORIAL,NUM_COPIAS,SINOPSIS,CATEGORIAS) values (432432,'3782','312321','312321',312312,'312312','Alegre');
 Insert into BIBLIOTECA.TODOSLIBROS (ID,TITULO,AUTOR,EDITORIAL,NUM_COPIAS,SINOPSIS,CATEGORIAS) values (21321312,'miyu','coco','coco',22,'oeowiewoiew','Educativo');
-Insert into BIBLIOTECA.TODOSLIBROS (ID,TITULO,AUTOR,EDITORIAL,NUM_COPIAS,SINOPSIS,CATEGORIAS) values (103,'Oracle','Miyu Maldonado','Coco',5,'Este libro es una introduccion a Oracle','Drama, Educativo');
 Insert into BIBLIOTECA.TODOSLIBROS (ID,TITULO,AUTOR,EDITORIAL,NUM_COPIAS,SINOPSIS,CATEGORIAS) values (6666,'Coco prueba mongo','hiram','uv',1,'Esto es una prueba para ver si se guarda la ruta de la imagen en mongo','Drama');
 Insert into BIBLIOTECA.TODOSLIBROS (ID,TITULO,AUTOR,EDITORIAL,NUM_COPIAS,SINOPSIS,CATEGORIAS) values (101,'Miyu el huerfanito','Coco UV','SEV',5,'Una novela dramatica que narra la vida de miyu','Drama, Novela');
-Insert into BIBLIOTECA.TODOSLIBROS (ID,TITULO,AUTOR,EDITORIAL,NUM_COPIAS,SINOPSIS,CATEGORIAS) values (6075509,'La hipótesis del amor','Ali Hazelwood','Alianza de Novela',5,'La hipótesis del amor sigue a Olive, personaje principal, en su intento de convencer a su mejor amiga, Anh, de que piense que su vida amorosa va muy bien. Para convencer a Anh, finge salir con su profesor, Adam. Olive y Adam intentan convencer a todos los que les rodean de que están enamorados. Pero mientras convencen a todos, olvidan que se supone que sus sentimientos son falsos. ¿Cuánto tiempo les tomará a Olive y Adam darse cuenta de que ya no quieren fingir?','Romance');
 Insert into BIBLIOTECA.TODOSLIBROS (ID,TITULO,AUTOR,EDITORIAL,NUM_COPIAS,SINOPSIS,CATEGORIAS) values (312564,'prueba123','miyu','dhejw',5,'fhjgdshj','Alegre');
 Insert into BIBLIOTECA.TODOSLIBROS (ID,TITULO,AUTOR,EDITORIAL,NUM_COPIAS,SINOPSIS,CATEGORIAS) values (978142158,'Goodnight Punpun','Inio Asano','Viz Media',10,'Punpun mira el mundo con los ojos de un pollito, porque tal vez así puede soportar que su amado padre sea un alcohólico violento; su madre, una víctima golpeada; y el amor de su vida, una compañera de clase que se marcha lejos','Drama, Recuentos de la vida');
 Insert into BIBLIOTECA.TODOSLIBROS (ID,TITULO,AUTOR,EDITORIAL,NUM_COPIAS,SINOPSIS,CATEGORIAS) values (3165267,'Miyucoco','Coco','Miyuki',4,'Blanca','Novela, Drama, Coco');
 Insert into BIBLIOTECA.TODOSLIBROS (ID,TITULO,AUTOR,EDITORIAL,NUM_COPIAS,SINOPSIS,CATEGORIAS) values (1232,'prueba libro con foto','prueba','prueba',44,'esto es una prueba','Prueba');
+Insert into BIBLIOTECA.TODOSLIBROS (ID,TITULO,AUTOR,EDITORIAL,NUM_COPIAS,SINOPSIS,CATEGORIAS) values (6075509,'La hipótesis del amor','Ali Hazelwood','Alianza de Novela',4,'La hipótesis del amor sigue a Olive, personaje principal, en su intento de convencer a su mejor amiga, Anh, de que piense que su vida amorosa va muy bien. Para convencer a Anh, finge salir con su profesor, Adam. Olive y Adam intentan convencer a todos los que les rodean de que están enamorados. Pero mientras convencen a todos, olvidan que se supone que sus sentimientos son falsos. ¿Cuánto tiempo les tomará a Olive y Adam darse cuenta de que ya no quieren fingir?','Romance');
+Insert into BIBLIOTECA.TODOSLIBROS (ID,TITULO,AUTOR,EDITORIAL,NUM_COPIAS,SINOPSIS,CATEGORIAS) values (103,'Oracle editado','Miyu Maldonado','Coco',5,'Este libro es una introduccion a Oracle','Drama, Educativo');
 --------------------------------------------------------
 --  DDL for Index SYS_C008437
 --------------------------------------------------------
@@ -239,11 +249,14 @@ CREATE UNIQUE INDEX "BIBLIOTECA"."SYS_C008444" ON "BIBLIOTECA"."RESEÑAS" ("SYS_
     PCTFREE 10 INITRANS 2 MAXTRANS 255
   TABLESPACE "USERS" ;
 --------------------------------------------------------
---  DDL for Index SYS_C008445
+--  DDL for Index SYS_C008452
 --------------------------------------------------------
 
-CREATE UNIQUE INDEX "BIBLIOTECA"."SYS_C008445" ON "BIBLIOTECA"."PRESTAMOS" ("SYS_NC_OID$")
+CREATE UNIQUE INDEX "BIBLIOTECA"."SYS_C008452" ON "BIBLIOTECA"."PRESTAMOS" ("SYS_NC_OID$")
     PCTFREE 10 INITRANS 2 MAXTRANS 255
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1
+  BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
   TABLESPACE "USERS" ;
 --------------------------------------------------------
 --  DDL for Index SYS_FK0000071534N00009$
@@ -296,11 +309,14 @@ CREATE UNIQUE INDEX "BIBLIOTECA"."SYS_C008443" ON "BIBLIOTECA"."LIBROS" ("SYS_NC
   BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
   TABLESPACE "USERS" ;
 --------------------------------------------------------
---  DDL for Index SYS_C008445
+--  DDL for Index SYS_C008452
 --------------------------------------------------------
 
-CREATE UNIQUE INDEX "BIBLIOTECA"."SYS_C008445" ON "BIBLIOTECA"."PRESTAMOS" ("SYS_NC_OID$")
+CREATE UNIQUE INDEX "BIBLIOTECA"."SYS_C008452" ON "BIBLIOTECA"."PRESTAMOS" ("SYS_NC_OID$")
     PCTFREE 10 INITRANS 2 MAXTRANS 255
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1
+  BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
   TABLESPACE "USERS" ;
 --------------------------------------------------------
 --  DDL for Index SYS_C008444
@@ -382,6 +398,9 @@ ALTER TABLE "BIBLIOTECA"."LIBROS" ADD UNIQUE ("SYS_NC_OID$") RELY
 
 ALTER TABLE "BIBLIOTECA"."PRESTAMOS" ADD UNIQUE ("SYS_NC_OID$") RELY
   USING INDEX PCTFREE 10 INITRANS 2 MAXTRANS 255
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1
+  BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
   TABLESPACE "USERS"  ENABLE;
 --------------------------------------------------------
 --  Constraints for Table RESEÑAS
